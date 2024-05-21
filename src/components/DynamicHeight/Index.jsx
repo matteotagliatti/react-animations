9fe49f02ae82c4ec53dd6a90b47a2ae1a2f537be
Matteo Tagliatti <matteotagliatti@gmail.com>
2024-05-21 17:24:30 +0200

    2 animations done

---
+++ src/components/DynamicHeight/Index.jsx
@@ -0,0 +1,48 @@
+import { motion } from "framer-motion";
+import { useState, useRef, useEffect } from "react";
+import "./styles.css";
+
+export default function Example() {
+  const [showExtraContent, setShowExtraContent] = useState(false);
+  const [height, setHeight] = useState(0);
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        const rect = entry.target.getBoundingClientRect();
+
+        setHeight(rect.height);
+      }
+    });
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <button className="button" onClick={() => setShowExtraContent((b) => !b)}>
+        Toggle height
+      </button>
+      <motion.div animate={{ height }} className="element">
+        <div ref={elementRef} className="inner">
+          <h1>Fake Family Drawer</h1>
+          <p>
+            This is a fake family drawer. Animating height is tricky, but
+            satisfying when it works.
+          </p>
+          {showExtraContent ? (
+            <p>
+              This extra content will change the height of the drawer. Some even
+              more content to make the drawer taller and taller and taller...
+            </p>
+          ) : null}
+        </div>
+      </motion.div>
+    </div>
+  );
+}
